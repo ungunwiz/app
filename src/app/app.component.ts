0a4da49fa7379f330b99a2ab06328c87d8136451
Frederik Shull <zlyfer@pm.me>
2023-12-03 00:32:31 +0100

    design and lots of other stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,7 +30,7 @@ export class AppComponent implements OnInit {
     },
     {
       type: 'route',
-      title: 'Settings (WIP)',
+      title: 'Settings',
       url: '/settings',
       icon: 'settings',
     },
@@ -40,6 +40,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.onSystemDarkModechange();
+
+    this.generateDistances();
 
     Promise.all([
       this.getWeapons(),
@@ -58,6 +60,10 @@ export class AppComponent implements OnInit {
     });
   }
 
+  /* ---------------------------------- */
+  /*                 UI                 */
+  /* ---------------------------------- */
+
   onSystemDarkModechange() {
     const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
     prefersDark.addListener((mediaQuery) => {
@@ -65,7 +71,117 @@ export class AppComponent implements OnInit {
     });
   }
 
-  /* ------------ Pubg Data ----------- */
+  /* ---------------------------------- */
+  /*           DATA PROCESSING          */
+  /* ---------------------------------- */
+
+  distances: any = [];
+  calculatedDamageMultiplier: any = [];
+  calculatedDamageFalloffs: any = {};
+
+  generateDamageFallOffs() {
+    this.pubgData.weapons.forEach((weapon: any) => {
+      this.calculatedDamageFalloffs[weapon.name] = [];
+      this.distances.forEach((distance: any) => {
+        const { start, end } = this.generateDamageFallOff(weapon, distance);
+        this.calculatedDamageMultiplier.push({
+          weapon: weapon.name,
+          start: { distance: start.distance, multiplier: start.multiplier },
+          end: { distance: end.distance, multiplier: end.multiplier },
+        });
+        var multiplier: any = this.map(
+          distance,
+          start.distance,
+          end.distance,
+          start.multiplier,
+          end.multiplier
+        );
+        this.calculatedDamageFalloffs[weapon.name].push(multiplier);
+      });
+    });
+  }
+
+  generateDamageFallOff(weapon: any, distance: number) {
+    const damageFalloffs = this.pubgData.damageFalloffs.filter((entry: any) => {
+      return entry.weapon_name === weapon.name;
+    });
+    if (damageFalloffs.length === 0) {
+      return {
+        start: { distance: '0', multiplier: 1 },
+        end: { distance: '1000', multiplier: 1 },
+      };
+    } else if (damageFalloffs.length === 1) {
+      return {
+        start: { distance: '0', multiplier: 1 },
+        end: damageFalloffs[0],
+      };
+    }
+    damageFalloffs.sort((a: any, b: any) => a.distance - b.distance);
+    const last = damageFalloffs[damageFalloffs.length - 1];
+    if (last.distance < 1000) {
+      damageFalloffs.push({ distance: '1000', multiplier: last.multiplier });
+    }
+
+    let start: any = damageFalloffs[0] || { distance: '1', multiplier: 1 };
+    let end: any = damageFalloffs[1] || { distance: '1000', multiplier: 1 };
+    let endSelected = false;
+    damageFalloffs.forEach((damageFalloff: any) => {
+      if (distance > damageFalloff.distance) {
+        start = damageFalloff;
+      }
+      if (distance <= damageFalloff.distance && !endSelected) {
+        end = damageFalloff;
+        endSelected = true;
+      }
+    });
+
+    return { start, end };
+  }
+
+  calcDamage(weapon: any, options: any | null = null) {
+    let damage = weapon.damage;
+    if (options.distance) {
+      options.distance -= 1;
+      damage *= this.calculatedDamageFalloffs[weapon.name][options.distance];
+    }
+    if (options.area) {
+      // TODO: Implement:
+    }
+    if (options.weaponType) {
+      // TODO: Implement:
+    }
+
+    return damage;
+  }
+
+  /* ---------------------------------- */
+  /*          HELPER FUNCTIONS          */
+  /* ---------------------------------- */
+
+  map(
+    value: number,
+    start1: number,
+    stop1: number,
+    start2: number,
+    stop2: number
+  ) {
+    value = parseFloat(value.toString());
+    start1 = parseFloat(start1.toString());
+    stop1 = parseFloat(stop1.toString());
+    start2 = parseFloat(start2.toString());
+    stop2 = parseFloat(stop2.toString());
+    return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
+  }
+
+  generateDistances(start: number = 1, end: number = 1000) {
+    for (let i = start; i <= end; i++) {
+      this.distances.push(i);
+    }
+  }
+
+  /* ---------------------------------- */
+  /*              PUBG DATA             */
+  /* ---------------------------------- */
 
   getWeapons() {
     return new Promise((resolve, reject) => {
--- src/app/weaponprofiles/weaponprofiles.page.ts
+++ src/app/weaponprofiles/weaponprofiles.page.ts
@@ -1,6 +1,5 @@
 import { ChartConfiguration, ChartOptions, ChartType } from 'chart.js';
 import { Component, OnInit } from '@angular/core';
-import { PubgDataService } from '@service/pubgData.service';
 
 @Component({
   selector: 'app-weaponprofiles',
@@ -8,7 +7,7 @@ import { PubgDataService } from '@service/pubgData.service';
   styleUrls: ['./weaponprofiles.page.scss'],
 })
 export class WeaponProfilesPage implements OnInit {
-  constructor(private pubgDataService: PubgDataService) {}
+  constructor() {}
 
   loading = true;
   pubgData: any = {};
@@ -25,33 +24,71 @@ export class WeaponProfilesPage implements OnInit {
 
   selectedWeapon: any = [];
 
-  distances: any = [];
-  calculatedDamageMultiplier: any = [];
-  calculatedDamageFalloffs: any = {};
-
   /* -------------- Chart ------------- */
   lineChartData: ChartConfiguration<'line'>['data'] = {
-    labels: this.distances,
     datasets: [],
   };
   lineChartOptions: ChartOptions<'line'> = {
     responsive: true,
+    interaction: {
+      mode: 'index',
+      intersect: false,
+    },
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: true,
+        position: 'top',
+        labels: {
+          color: this.getStyleColor('--ion-color-step-400'),
+          font: {
+            size: 16,
+          },
+        },
+      },
+      tooltip: {
+        axis: 'x',
+        callbacks: {
+          label: function (context: any) {
+            var label = context.dataset.label || '';
+            if (label) {
+              label += ': ';
+            }
+            if (context.parsed.y !== null) {
+              label += context.parsed.y.toFixed(2);
+            }
+            return '  ' + label;
+          },
+        },
+      },
+    },
     scales: {
       x: {
         stacked: false,
         grid: {
           display: true,
-          color: '#ffffff',
+          color: this.getStyleColor('--ion-color-step-400'),
+          borderDash: [1, 2],
+          tickBorderDash: [1, 2],
+          lineWidth: 1,
         },
         type: 'linear',
+        min: 0,
+        max: 1000,
         title: {
           display: true,
           text: 'Distance',
+          color: this.getStyleColor('--ion-color-step-400'),
+
+          font: {
+            size: 20,
+          },
         },
         ticks: {
+          color: this.getStyleColor('--ion-color-step-400'),
           stepSize: 100,
           callback: function (value, index, values) {
-            return Number(value) % 100 === 0 ? value + 'm' : '';
+            return `${value}m`;
           },
         },
       },
@@ -59,11 +96,28 @@ export class WeaponProfilesPage implements OnInit {
         stacked: false,
         grid: {
           display: true,
-          color: '#ffffff',
+          color: this.getStyleColor('--ion-color-step-400'),
+          borderDash: [1, 2],
+          tickBorderDash: [1, 2],
+
+          lineWidth: 1,
         },
+        type: 'linear',
+        min: 0,
         title: {
           display: true,
           text: 'Damage',
+          color: this.getStyleColor('--ion-color-step-400'),
+          font: {
+            size: 20,
+          },
+        },
+        ticks: {
+          color: this.getStyleColor('--ion-color-step-400'),
+          stepSize: 25,
+          callback: function (value, index, values) {
+            return `${value}`;
+          },
         },
       },
     },
@@ -71,53 +125,7 @@ export class WeaponProfilesPage implements OnInit {
   lineChartLegend = true;
 
   ngOnInit() {
-    // this.weapon = this.activatedRoute.snapshot.paramMap.get(
-    //   'weapon'
-    // ) as string;
     this.pubgData = JSON.parse(localStorage.getItem('pubgData') || '{}');
-    this.generateDistances();
-    /* ------------ Debugging ----------- */
-    // console.debug(`this.pubgData.weapons:`, this.pubgData.weapons[7]);
-    // console.table(
-    //   `this.pubgData.damageFalloffs:`,
-    //   this.pubgData.damageFalloffs.filter((entry: any) => {
-    //     return entry.weapon_name === this.pubgData.weapons[7].name;
-    //   })
-    // );
-    // this.pubgData.weapons = this.pubgData.weapons.slice(0, 3);
-    // this.pubgData.damageFalloffs = this.pubgData.damageFalloffs.filter(
-    //   (entry: any) => {
-    //     return this.pubgData.weapons.find((weapon: any) => {
-    //       return weapon.name === entry.weapon_name;
-    //     });
-    //   }
-    // );
-    /* ---------------- - --------------- */
-    this.pubgData.weapons.forEach((weapon: any) => {
-      this.calculatedDamageFalloffs[weapon.name] = [];
-      this.distances.forEach((distance: any) => {
-        const { start, end } = this.generateDamageFallOffs(weapon, distance);
-        // console.debug(
-        //   `${start.distance <= distance && distance <= end.distance},d,s,e`,
-        //   distance,
-        //   start.distance,
-        //   end.distance
-        // );
-        this.calculatedDamageMultiplier.push({
-          weapon: weapon.name,
-          start: { distance: start.distance, multiplier: start.multiplier },
-          end: { distance: end.distance, multiplier: end.multiplier },
-        });
-        var multiplier: any = this.map(
-          distance,
-          start.distance,
-          end.distance,
-          start.multiplier,
-          end.multiplier
-        );
-        this.calculatedDamageFalloffs[weapon.name].push(multiplier);
-      });
-    });
 
     this.loading = false;
   }
@@ -144,6 +152,53 @@ export class WeaponProfilesPage implements OnInit {
     return this.selectedWeapon.includes(weapon);
   }
 
+  addGraph(weapon: any) {
+    var data: any = [];
+    const color =
+      this.colors[this.lineChartData.datasets.length % this.colors.length];
+
+    const weaponDamageFalloff = this.pubgData.damageFalloffs.filter(
+      (entry: any) => {
+        return entry.weapon_name === weapon.name;
+      }
+    );
+
+    if (weaponDamageFalloff.length === 0) {
+      data.push({
+        x: 0,
+        y: weapon.damage,
+      });
+    } else {
+      weaponDamageFalloff.forEach((damageFalloff: any) => {
+        data.push({
+          x: damageFalloff.distance,
+          y: damageFalloff.multiplier * weapon.damage,
+        });
+      });
+    }
+    if (data[data.length - 1].x < 1000) {
+      data.push({
+        x: 1000,
+        y: data[data.length - 1].y,
+      });
+    }
+
+    this.lineChartData.datasets.push({
+      data,
+      label: weapon.name,
+      fill: false,
+      tension: 0,
+      borderColor: color,
+      backgroundColor: `#ffffff00`,
+      borderWidth: 3,
+      pointRadius: 0,
+      // borderDash: [
+      //   3,
+      //   this.lineChartData.datasets.length >= this.colors.length ? 1 : 0,
+      // ],
+    });
+  }
+
   updateGraph() {
     this.lineChartData.datasets = [];
     this.selectedWeapon.forEach((weapon: any) => {
@@ -152,115 +207,10 @@ export class WeaponProfilesPage implements OnInit {
     this.lineChartData = { ...this.lineChartData };
   }
 
-  addGraph(weapon: any) {
-    var transparency: number = 1 / this.pubgData.weapons.length;
-    transparency +=
-      this.lineChartData.datasets.length * (1 / this.pubgData.weapons.length);
-
-    var color = null;
-
-    if (this.lineChartData.datasets.length < this.colors.length) {
-      color = this.colors[this.lineChartData.datasets.length];
-    } else {
-      color =
-        this.colors[this.lineChartData.datasets.length % this.colors.length];
-    }
-
-    var dataset: any = {
-      data: [],
-      label: weapon.name,
-      fill: false,
-      tension: 0.5,
-      borderColor: color,
-      backgroundColor: `#ffffff00`,
-      borderWidth: 3,
-      pointRadius: 0,
-      pointBackgroundColor: 'white',
-      pointBorderWidth: 0,
-      borderDash: [
-        3,
-        this.lineChartData.datasets.length >= this.colors.length ? 1 : 0,
-      ],
-    };
-
-    this.distances.forEach((distance: any) => {
-      dataset.data.push(this.calcDamage(weapon, { distance }));
-    });
-    this.lineChartData.datasets.push(dataset);
-    console.debug(`dataset:`, dataset);
-  }
-
-  calcDamage(weapon: any, options: any | null = null) {
-    let damage = weapon.damage;
-    if (options.distance) {
-      options.distance -= 1;
-      damage *= this.calculatedDamageFalloffs[weapon.name][options.distance];
-    }
-    if (options.area) {
-      // TODO: Implement:
-    }
-    if (options.weaponType) {
-      // TODO: Implement:
-    }
-
-    return damage;
-  }
-
-  generateDamageFallOffs(weapon: any, distance: number) {
-    const damageFalloffs = this.pubgData.damageFalloffs.filter((entry: any) => {
-      return entry.weapon_name === weapon.name;
-    });
-    if (damageFalloffs.length === 0) {
-      return {
-        start: { distance: '0', multiplier: 1 },
-        end: { distance: '1000', multiplier: 1 },
-      };
-    } else if (damageFalloffs.length === 1) {
-      return {
-        start: { distance: '0', multiplier: 1 },
-        end: damageFalloffs[0],
-      };
-    }
-    damageFalloffs.sort((a: any, b: any) => a.distance - b.distance);
-    const last = damageFalloffs[damageFalloffs.length - 1];
-    if (last.distance < 1000) {
-      damageFalloffs.push({ distance: '1000', multiplier: last.multiplier });
-    }
-
-    let start: any = damageFalloffs[0] || { distance: '1', multiplier: 1 };
-    let end: any = damageFalloffs[1] || { distance: '1000', multiplier: 1 };
-    let endSelected = false;
-    damageFalloffs.forEach((damageFalloff: any) => {
-      if (distance > damageFalloff.distance) {
-        start = damageFalloff;
-      }
-      if (distance <= damageFalloff.distance && !endSelected) {
-        end = damageFalloff;
-        endSelected = true;
-      }
-    });
-
-    return { start, end };
-  }
-
-  map(
-    value: number,
-    start1: number,
-    stop1: number,
-    start2: number,
-    stop2: number
-  ) {
-    value = parseFloat(value.toString());
-    start1 = parseFloat(start1.toString());
-    stop1 = parseFloat(stop1.toString());
-    start2 = parseFloat(start2.toString());
-    stop2 = parseFloat(stop2.toString());
-    return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
-  }
-
-  generateDistances(start: number = 1, end: number = 1000) {
-    for (let i = start; i <= end; i++) {
-      this.distances.push(i);
-    }
+  getStyleColor(variable: string) {
+    const color = getComputedStyle(document.documentElement).getPropertyValue(
+      variable
+    );
+    return color;
   }
 }
