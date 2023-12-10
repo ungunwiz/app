3027f83b468847332c70bf969a34ba152b62df56
Frederik Shull <zlyfer@pm.me>
2023-12-10 15:27:07 +0100

    changed name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,11 +2,11 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'unknownwiz',
+  appName: 'ungunwiz',
   webDir: 'www',
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: 'https',
+  },
 };
 
 export default config;
