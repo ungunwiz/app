89ae84ee066a307fb0462c3c7ab9cd4171282e7e
Frederik Shull <zlyfer@pm.me>
2023-12-15 22:39:35 +0100

    android & ios & meta

---
+++ android/app/src/main/java/app/ungunwiz/gg/MainActivity.java
@@ -0,0 +1,5 @@
+package app.ungunwiz.gg;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'app.ungunwiz.gg',
   appName: 'ungunwiz',
   webDir: 'www',
   server: {
